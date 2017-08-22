e6a00618439477b43ff3fe5e6a4d7b9b4dc8ae32
JoseLuisSPG <joseluis.sanchezporro.godoy@gmail.com>
2017-08-22 08:41:30 +0200

    Initial Commit

---
+++ Westeros/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Westeros
+//
+//  Created by jose luis sanchez-porro godoy on 22/08/2017.
+//  Copyright © 2017 jose luis sanchez-porro godoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
